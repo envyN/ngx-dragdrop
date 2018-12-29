5f1c32b694f2b5682120106a9b02c44443e9d49d
Naveen Adarsh Petla <npetla@vmware.com>
2018-12-29 19:36:36 +0530

    Adding dist files to gh-pages branch

--- projects/ngx-dragdrop/src/public_api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of ngx-dragdrop
- */
-
-export * from './lib/dragDrop.service';
-export * from './lib/draggable.directive';
-export * from './lib/droppable.directive';
-export * from './lib/ngx-dragdrop.module';
